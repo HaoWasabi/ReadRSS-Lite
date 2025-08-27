6e8a00cba2bae79027a9309eaa71ef0090bde123
HaoWasabi <truonggiahao24@gmail.com>
2025-08-27 14:10:35 +0700

    feat(cogs): implement Events class with on_ready method

---
+++ src/cogs/events.py
@@ -0,0 +1,24 @@
+import logging
+from nextcord.ext import commands
+
+logger = logging.getLogger("Events")
+
+
+class Events(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        super().__init__(bot)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info(f"Bot {self.bot.user} is ready")
+        logger.info("Current commands: %s", str([command.name for command in self.bot.commands]))
+        logger.info("Current slash commands: %s", str([command.name for command in self.bot.get_application_commands()]))
+
+        await self.bot.sync_all_application_commands()
+        logger.info(f'Bot {self.bot.user} is ready and commands are synced.')
+
+        if not self.push_noti.is_running():
+            self.push_noti.start()
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Events(bot))
